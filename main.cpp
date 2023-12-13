dc62f931f4ec88004b545614a7f101ed3071383c
DavidBakalov21 <davidbakalov33@gmail.com>
2023-12-13 11:12:10 +0200

    paddle moving

--- main.cpp
+++ main.cpp
@@ -1,37 +1,26 @@
 #include <SFML/Graphics.hpp>
 #include <iostream>
-
-class Game {
-public:
-    Game() {}
-
-    void handleInput(sf::RenderWindow& window) {
-        sf::Event event;
-        while (window.pollEvent(event)) {
-            if (event.type == sf::Event::Closed) {
-                window.close();
-            }
-        }
-    }
-
-private:
-};
-
 class Paddle {
 public:
     double speed;
     double size;
     double xPos;
-    double yPos;
+    double yPos=20;
 };
 
 class PaddlePlayer : public Paddle {
 public:
-    void moveDown() {
-        yPos=-speed;
-    }
     void moveUp() {
-        yPos =+speed;
+        if (yPos > 0) {
+            yPos -= 20;
+        }
+
+    }
+    void moveDown() {
+        if (yPos < 610) {
+            yPos += 20;
+        }
+      
     }
 };
 
@@ -72,6 +61,33 @@ public:
         Score += score;
     }
 };
+class Game {
+public:
+    Game() {}
+
+    void handleInput(sf::RenderWindow& window, PaddlePlayer& player) {
+        sf::Event event;
+        while (window.pollEvent(event)) {
+            if (event.type == sf::Event::Closed) {
+                window.close();
+            }
+            if (event.type == sf::Event::KeyPressed) {
+                if (event.key.code == sf::Keyboard::Up) {
+                    player.moveUp();
+                }
+                else if (event.key.code == sf::Keyboard::Down) {
+                    player.moveDown();
+                }
+
+                else if (event.key.code == sf::Keyboard::B) {
+                    window.close();
+                }
+            }
+        }
+    }
+};
+
+
 
 class Settings {
 public:
@@ -96,37 +112,35 @@ private:
 
 class Renderer {
 public:
-    Renderer(Game& game, Settings& settings) : game(game), settings(settings) {}
+    Renderer(Game& game, Settings& settings, PaddlePlayer& player) : game(game), settings(settings), player(player) {}
 
     void handleInput(sf::RenderWindow& window) {
-        game.handleInput(window);
+        game.handleInput(window, player);
     }
 
     void render(sf::RenderWindow& window) {
         window.clear();
-
-        // Render game elements
-        sf::RectangleShape rectangle(sf::Vector2f(20.f, 90.f));
-        rectangle.setPosition(50.f, 275.f);
-        rectangle.setFillColor(sf::Color::Green);
-        window.draw(rectangle);
-
-        // Render settings UI
-       
-
+        sf::RectangleShape playerPaddle(sf::Vector2f(20.f, 90.f));
+        playerPaddle.setPosition(50.f, player.yPos);
+        playerPaddle.setFillColor(sf::Color::Green);
+        window.draw(playerPaddle);
         window.display();
     }
 
 private:
     Game& game;
     Settings settings;
+    PaddlePlayer& player;
 };
+
 
 int main() {
     sf::RenderWindow window(sf::VideoMode(1700, 700), "Ping pong");
+   // sf::RenderWindow window2(sf::VideoMode(700, 700), "Settings");
     Game game;
     Settings settings;
-    Renderer renderer(game, settings);
+    PaddlePlayer player; 
+    Renderer renderer(game, settings, player);
     while (window.isOpen()) {
         renderer.handleInput(window);
         renderer.render(window);
