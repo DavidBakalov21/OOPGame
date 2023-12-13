83bbcba5c5a9f1fbe2342c4ea87dfba2d40aa4c6
DavidBakalov21 <davidbakalov33@gmail.com>
2023-12-13 19:43:45 +0200

    ball and score

--- main.cpp
+++ main.cpp
@@ -1,54 +1,9 @@
 #include <SFML/Graphics.hpp>
 #include <iostream>
-class Paddle {
-public:
-    double speed;
-    double size;
-    double xPos;
-    double yPos=20;
-};
-
-class PaddlePlayer : public Paddle {
-public:
-    void moveUp() {
-        if (yPos > 0) {
-            yPos -= 20;
-        }
-
-    }
-    void moveDown() {
-        if (yPos < 610) {
-            yPos += 20;
-        }
-      
-    }
-};
-
-class PaddleEnemy : public Paddle {
-public:
-};
-
-class Ball {
-public:
-    double xPos;
-    double yPos;
-    double Speedy;
-    double Speedx;
-    void move() {
-        xPos += Speedx;
-        yPos += Speedy;
-    }
-
-    void ChangeSpeed(double XChange, double YChange) {
-        Speedx += XChange;
-        Speedy += YChange;
-    }
-};
-
 class PlayerData {
 public:
     std::string PlayerName;
-    int Score;
+    int Score = 0;
     void SetScore(int score) {
         Score += score;
     }
@@ -61,27 +16,101 @@ public:
         Score += score;
     }
 };
+class Paddle {
+public:
+    double speed;
+    double size;
+    double xPos;
+    double yPos=20;
+};
+
+class PaddlePlayer : public Paddle {
+public:
+    void moveUp() {
+        if (yPos > 0) {
+            yPos -= 1.3;
+        }
+
+    }
+    void moveDown() {
+        if (yPos < 610) {
+            yPos += 1.3;
+        }
+      
+    }
+};
+
+class PaddleEnemy : public Paddle {
+public:
+};
+
+class Ball {
+public:
+    double xPos=300;
+    double yPos=300;
+    double Speedy=1;
+    double Speedx=1;
+
+
+    bool collidesWithPlayer(const PaddlePlayer& player) const {
+        sf::FloatRect ballBounds(xPos - 30.f, yPos - 30.f, 60.f, 60.f);
+        sf::FloatRect playerBounds(50.f, static_cast<float>(player.yPos), 20.f, 90.f);
+
+        return ballBounds.intersects(playerBounds);
+    }
+
+    void move(const PaddlePlayer& player, PlayerData& Data) {
+        xPos += Speedx;
+        yPos += Speedy;
+        if (yPos>610 || yPos<0)
+        {
+            Speedy *= -1;
+        }
+        if (xPos > 1650) {
+            std::cout << "Bot fail";
+            Speedx *= -1;
+           
+        }
+        if (xPos < 0)
+        {
+            std::cout << "Player fail";
+            Data.SetScore(-1);
+            Speedx *= -1;
+        }
+        if (collidesWithPlayer(player)) {
+            std::cout << "Player hit!" << std::endl;         
+            Speedx *= -1; 
+            Data.SetScore(1);
+        }
+
+    }
+
+    void ChangeSpeed(double XChange, double YChange) {
+        Speedx += XChange;
+        Speedy += YChange;
+    }
+};
+
+
 class Game {
 public:
     Game() {}
 
     void handleInput(sf::RenderWindow& window, PaddlePlayer& player) {
+        if (sf::Keyboard::isKeyPressed(sf::Keyboard::Up)) {
+            player.moveUp();
+        }
+        else if (sf::Keyboard::isKeyPressed(sf::Keyboard::Down)) {
+            player.moveDown();
+        }
+
         sf::Event event;
         while (window.pollEvent(event)) {
             if (event.type == sf::Event::Closed) {
                 window.close();
             }
-            if (event.type == sf::Event::KeyPressed) {
-                if (event.key.code == sf::Keyboard::Up) {
-                    player.moveUp();
-                }
-                else if (event.key.code == sf::Keyboard::Down) {
-                    player.moveDown();
-                }
-
-                else if (event.key.code == sf::Keyboard::B) {
-                    window.close();
-                }
+            else if (event.type == sf::Event::KeyPressed && event.key.code == sf::Keyboard::B) {
+                window.close();
             }
         }
     }
@@ -112,7 +141,7 @@ private:
 
 class Renderer {
 public:
-    Renderer(Game& game, Settings& settings, PaddlePlayer& player) : game(game), settings(settings), player(player) {}
+    Renderer(Game& game, Settings& settings, PaddlePlayer& player, Ball& ball, PlayerData& playerData) : game(game), settings(settings), player(player), ball(ball), playerData(playerData) {}
 
     void handleInput(sf::RenderWindow& window) {
         game.handleInput(window, player);
@@ -123,26 +152,51 @@ public:
         sf::RectangleShape playerPaddle(sf::Vector2f(20.f, 90.f));
         playerPaddle.setPosition(50.f, player.yPos);
         playerPaddle.setFillColor(sf::Color::Green);
+
+        sf::CircleShape Gameball(30.f);
+        Gameball.setPosition(ball.xPos, ball.yPos);
+
+
+
+        sf::Font font;
+        if (!font.loadFromFile("C:\\Users\\Давід\\source\\repos\\OOPGame\\x64\\Debug\\Arial.ttf")) {
+            return;
+        }
+
+        sf::Text playerInfoText;
+        playerInfoText.setFont(font);
+        playerInfoText.setCharacterSize(20);
+        playerInfoText.setFillColor(sf::Color::White);
+        playerInfoText.setPosition(10.f, 10.f);
+        playerInfoText.setString("Player: " + playerData.PlayerName + " Score: " + std::to_string(playerData.Score));
+
+        window.draw(playerInfoText);
+
         window.draw(playerPaddle);
+        window.draw(Gameball);
         window.display();
     }
 
 private:
+    Ball& ball;
     Game& game;
     Settings settings;
     PaddlePlayer& player;
+    PlayerData& playerData;
 };
 
 
 int main() {
     sf::RenderWindow window(sf::VideoMode(1700, 700), "Ping pong");
-   // sf::RenderWindow window2(sf::VideoMode(700, 700), "Settings");
     Game game;
     Settings settings;
     PaddlePlayer player; 
-    Renderer renderer(game, settings, player);
+    Ball ball;
+    PlayerData playerData;
+    Renderer renderer(game, settings, player,ball, playerData);
     while (window.isOpen()) {
         renderer.handleInput(window);
+        ball.move(player, playerData);
         renderer.render(window);
     }
 
